f0cb8f544731a4806af680969890363404d15f04
Daniel Hoppe Alvarez <dhoppealvarez@gmail.com>
2019-01-22 14:12:45 +0100

    Update DKProEndpoint.java

--- src/main/java/com/DKProEndpoint.java
+++ src/main/java/com/DKProEndpoint.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.StringWriter;
-
+// TODO: Switch to post request 
 @RestController
 public class DKProEndpoint {
 
